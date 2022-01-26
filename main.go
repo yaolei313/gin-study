f4ea4cc30e3fd04451c68c28c7a4e5a34a4a773c
yaolei03 <yaolei03@kuaishou.com>
2022-01-26 20:30:48 +0800

    add code

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
-	r := gin.Default()
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	// gin.DefaultWriter = io.MultiWriter(f) 只写文件
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	// gin.Default With the Logger and Recovery middleware already attached
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.CustomRecovery(func(c *gin.Context, err interface{}) {
+		// type assertion https://go.dev/tour/methods/15
+		errStr, ok := err.(string)
+		if ok {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", errStr))
+		}
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
+
+
 	r.Static("/static", "./assets")
 	r.StaticFile("/favicon.ico", "./resources/favicon.ico")
 
@@ -33,6 +53,10 @@ func main() {
 		message := name + " is " + action
 		fullPath := c.FullPath()
 		c.String(http.StatusOK, "%v %v", message, fullPath)
+	})
+
+	r.GET("/panic", func(c *gin.Context) {
+		panic("test panic")
 	})
 
 	// by default, it serves on :8080 unless a PORT environment variable was defined.
